3a9173c6c226299d19c98f56a53ac78c9323110d
ltyohannes955 <ltyohannes95@gmail.com>
2025-06-30 23:50:37 +0300

    added cors for deploying on render

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,22 @@ import { logger } from "hono/logger";
 import connectDB from "./db/connect";
 import authRoutes from "./routes/auth.routes";
 import userRoutes from "./routes/user.routes";
+import { cors } from "hono/cors";
 
 const app = new Hono();
 
+app.use(
+  "*",
+  cors({
+    origin: (origin) => {
+      const allowed = ["http://localhost:3000"];
+      return allowed.includes(origin ?? "") ? origin : "";
+    },
+    credentials: true,
+  })
+);
 app.use(poweredBy());
 app.use(logger());
-
 
 // Mount routes
 app.route("/auth", authRoutes);
